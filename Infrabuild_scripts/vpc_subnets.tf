cad09a8cf46556e2ea068e72592b733a67350142
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-06-16 00:24:22 +0530

    Update vpc_subnets.tf

--- Infrabuild_scripts/vpc_subnets.tf
+++ Infrabuild_scripts/vpc_subnets.tf
@@ -39,6 +39,7 @@ resource "aws_route" "private_route3" {
   destination_cidr_block = var.private_subnet3
   gateway_id             = aws_internet_gateway.igw.id
 }
+
 resource "aws_subnet" "privatesubnet1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnet1
@@ -61,4 +62,24 @@ resource "aws_subnet" "publicsubnet1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet1
   tags       = merge(var.tagging, { Name = "Public-Subnet1" })
+}
+
+resource "aws_route_table_association" "private_subnet1_assoc" {
+  subnet_id      = aws_subnet.privatesubnet1.id
+  route_table_id = aws_route_table.privatert.id
+}
+
+resource "aws_route_table_association" "private_subnet2_assoc" {
+  subnet_id      = aws_subnet.privatesubnet2.id
+  route_table_id = aws_route_table.privatert.id
+}
+
+resource "aws_route_table_association" "private_subnet3_assoc" {
+  subnet_id      = aws_subnet.privatesubnet3.id
+  route_table_id = aws_route_table.privatert.id
+}
+
+resource "aws_route_table_association" "public_subnet3_assoc" {
+  subnet_id      = aws_subnet.publicsubnet1.id
+  route_table_id = aws_route_table.publicrt.id
 }
