46b3c14445a25089f17d5f1311fa6c8f25542b20
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-06-15 23:15:14 +0530

    Update variables.tf

--- Infrabuild_scripts/variables.tf
+++ Infrabuild_scripts/variables.tf
@@ -3,6 +3,9 @@
 variable "region" {
   description = "The region where the services will be getting provisioned"
 }
+variable "vpc_cidr"{
+  description = "VPC CIDR range"
+}
 variable "instance_type" {
   description = "VM specification"
 }
