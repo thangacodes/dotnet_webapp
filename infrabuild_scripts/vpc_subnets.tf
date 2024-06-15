312cb488243bb1d257eed19abc739faea806116a
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-06-15 23:00:21 +0530

    Delete infrabuild_scripts directory

--- infrabuild_scripts/vpc_subnets.tf
+++
@@ -1,6 +0,0 @@
-## This script is use to create AWS VPC and it require components like Internet Gateway, Private/Public Subnets, RouteTable..
-
-resource "aws_vpc" "main" {
- cidr_block = var.vpc_cidr
-  tags= merge(var.tagging, { Name="Dev-Account"}
-}
