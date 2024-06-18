d1e672cb0ed96417e8249a80dd99dc54257c9319
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-06-18 07:59:14 +0530

    Update backend.tf

--- infrabuild_scripts/backend.tf
+++ infrabuild_scripts/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "project-cicd-tf-eks"
     key            = "eks/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-state-lock"
   }
